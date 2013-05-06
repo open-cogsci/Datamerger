3506bf7da2933aa4552a9b8ae925965dca1af0e0
Daniel Schreij <dschreij@gmail.com>
2013-05-06 12:05:03 +0200

    Bugfix + datatype correction
    - Corrected for KeyError when writing xls(x) files if one file misses a
    column compared to the others
    - Data is now written to xls(x) files in correct datatype, instead of
    always as string

--- libdatamerger/datamerger_ui.py
+++ libdatamerger/datamerger_ui.py
@@ -21,7 +21,7 @@ import sheet_io_tools
 from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtWebKit import QWebView
 
-version = 1.0
+version = 1.01
 author = "Daniel Schreij"
 email = "d.schreij@vu.nl"
 
